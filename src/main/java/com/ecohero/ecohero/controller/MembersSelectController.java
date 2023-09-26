f0cf8a330676b150efa404a1eb60fe367e62728e
11yhj <uouo_@naver.com>
2023-09-26 19:19:32 +0900

    localhost수정완료

--- src/main/java/com/ecohero/ecohero/controller/MembersSelectController.java
+++ src/main/java/com/ecohero/ecohero/controller/MembersSelectController.java
@@ -3,11 +3,13 @@ package com.ecohero.ecohero.controller;
 import com.ecohero.ecohero.dao.MembersSelectDAO;
 import com.ecohero.ecohero.vo.MembersVO;
 import org.springframework.stereotype.Controller;
+
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+
 
 @Controller
 @RequestMapping("/members")
@@ -16,7 +18,7 @@ public class MembersSelectController {
     public String MembersSelect(Model model) {
         MembersSelectDAO dao = new MembersSelectDAO();
         List<MembersVO> mbsvo = dao.selectMembersInfo();
-        model.addAttribute ("USER_ID", mbsvo);
+        model.addAttribute ("userId", mbsvo);
         return "thymeleafEcoHero/membersSelect";
     }
 }
--- src/main/java/com/ecohero/ecohero/dao/MembersSelectDAO.java
+++ src/main/java/com/ecohero/ecohero/dao/MembersSelectDAO.java
@@ -20,19 +20,19 @@ public class MembersSelectDAO {
         List<MembersVO> mvl = new ArrayList<>();
         try{
             conn = Common.getConnection();
-            String sql = "SELECT USER_ID, USER_ALIAS, USER_NAME, HERO_EMAIL, USER_PHONE, TO_CHAR(JOIN_DATE, 'YYYY-MM-DD') JOIN_DATE, HERO_GRADE, USER_POINT, TRUNC(SYSDATE - JOIN_DATE) DAYS\n" +
-                    "FROM MEMBERS M JOIN GRADE G\n" +
-                    "ON M.USER_POINT BETWEEN G.LO_POINT AND G.HI_POINT;\n";
+            String sql = "SELECT USER_ID, USER_ALIAS, USER_NAME, USER_EMAIL, USER_PHONE, TO_CHAR(JOIN_DATE, 'YYYY-MM-DD') JOIN_DATE, HERO_GRADE, USER_POINT, TRUNC(SYSDATE - JOIN_DATE) DAYS\n" +
+                    "                    FROM MEMBERS M JOIN GRADE G\n" +
+                    "                    ON M.USER_POINT BETWEEN G.LO_POINT AND G.HI_POINT";
             pstmt = conn.prepareStatement(sql);
             rs = pstmt.executeQuery();
 
             while(rs.next()){
-                 String userId = rs.getNString("USER_ID");
-                 String userAlias = rs.getNString("USER_ALIAS");
-                 String userName = rs.getNString("USER_NAME");
-                 String userEmail = rs.getNString("HERO_EMAIL");
-                 String userPhone = rs.getNString("USER_PHONE");
-                 String joinDate = rs.getNString("JOIN_DATE");
+                 String userId = rs.getString("USER_ID");
+                 String userAlias = rs.getString("USER_ALIAS");
+                 String userName = rs.getString("USER_NAME");
+                 String userEmail = rs.getString("USER_EMAIL");
+                 String userPhone = rs.getString("USER_PHONE");
+                 String joinDate = rs.getString("JOIN_DATE");
                  String heroGrade = rs.getString("HERO_GRADE");
                  int userPoint = rs.getInt("USER_POINT");
                  int days = rs.getInt("DAYS");
