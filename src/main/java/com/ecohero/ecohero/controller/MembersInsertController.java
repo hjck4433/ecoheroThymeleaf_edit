b471db8d54916fb51297e777eed7bb119de84c12
lumiereJE <lumiere_remplir@naver.com>
2023-09-26 17:42:50 +0900

    MembersInsert

--- src/main/java/com/ecohero/ecohero/controller/MembersInsertController.java
+++ src/main/java/com/ecohero/ecohero/controller/MembersInsertController.java
@@ -1,5 +1,27 @@
 package com.ecohero.ecohero.controller;
 
-public class MembersInsertController {
 
+import com.ecohero.ecohero.dao.MembersInsertDAO;
+import com.ecohero.ecohero.vo.MembersVO;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/members")
+public class MembersInsertController {
+    @GetMapping("/insert")
+    public String insertViewMembers(Model model) {
+        model.addAttribute("userId", new MembersVO());
+        return "thymeleafEcoHero/membersInsert";
+    }
+    @PostMapping("/insert")
+    public String insertDBMembers(@ModelAttribute("userId") MembersVO membersVO) {
+        MembersInsertDAO mInsertDao = new MembersInsertDAO();
+        mInsertDao.MembersJoin(membersVO);
+        return "thymeleafEcoHero/membersInsertRst";
+    }
 }
