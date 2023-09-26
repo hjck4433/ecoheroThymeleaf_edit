82d213c02c519f8c1cc151b2ec89e6af61f9b0e9
11yhj <uouo_@naver.com>
2023-09-26 17:28:27 +0900

    membersSelectController 수정

--- src/main/java/com/ecohero/ecohero/controller/MembersSelectController.java
+++ src/main/java/com/ecohero/ecohero/controller/MembersSelectController.java
@@ -1,43 +1,27 @@
 package com.ecohero.ecohero.controller;
 
+import com.ecohero.ecohero.dao.MembersSelectDAO;
+import com.ecohero.ecohero.vo.MembersVO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-@RequestMapping("/emp")
-public class MembersSelectController {
-    @GetMapping("/select")
-    public String selectEmp() {
-        System.out.println("여기는 emp select호출문입니다.");
-        return "test";
-
 import java.util.List;
 
-        @Controller
-        @RequestMapping("/emp")
-        public class EmpController {
-            @GetMapping("/select")
-            public String selectEmp(Model model) { // View로 모델을 넘겨주는 객체
-                EmpDAO dao = new EmpDAO();
-                List<EmpVO> emps = dao.empSelect();
-                model.addAttribute("employees", emps);
-                return "thymeleafEx/empSelect";
-            }
-
-            @GetMapping("/insert")
-            public String insertViewEmp(Model model) { // View로 모델을 넘겨주는 객체
-                model.addAttribute("employees", new EmpVO()); // 빈 객체를 넘겨 줌
-                return "thymeleafEx/empInsert";
-            }
-
-            @PostMapping("/insert")
-            public String insertDBEmp(@ModelAttribute("employees") EmpVO empVO) {
-                EmpDAO dao = new EmpDAO();
-                dao.empInsert(empVO);
-                return "thymeleafEx/empInsertRst";
-
-            }
-
-        }
+@Controller
+@RequestMapping("/members")
+public class MembersSelectController {
+    @GetMapping("/select")
+    public String MembersSelect(Model model) {
+        MembersSelectDAO dao = new MembersSelectDAO();
+        List<MembersVO> mbsvo = dao.selectMembersInfo();
+        model.addAttribute ("USER_ID", mbsvo);
+        return "thymeleafEcoHero/membersSelect";
+    }
 }
+
+//public String selectEmp(Model model) { // View로 모델을 넘겨주는 객체
+//        EmpDAO dao = new EmpDAO();
+//        List<EmpVO> emps = dao.empSelect();
+//        model.addAttribute("employees", emps);
--- src/main/java/com/ecohero/ecohero/dao/MembersSelectDAO.java
+++ src/main/java/com/ecohero/ecohero/dao/MembersSelectDAO.java
@@ -1,4 +1,4 @@
-package Project_Ecohero.Dao;
+package com.ecohero.ecohero.dao;
 
 import com.ecohero.ecohero.common.Common;
 import com.ecohero.ecohero.vo.MembersVO;
@@ -7,23 +7,20 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 
-public class MembersDao {
+
+public class MembersSelectDAO {
 
     Connection conn = null;
     PreparedStatement pstmt = null;
     ResultSet rs = null;
-    Scanner sc = new Scanner(System.in);
 
-    // 회원가입 시 중복 체크를 위해 모든 회원 정보 불러오기 / 단, 비밀번호 및 가입시 자동 입력 되는 항목 제외
     public List<MembersVO> selectMembersInfo(){
         List<MembersVO> mvl = new ArrayList<>();
         try{
             conn = Common.getConnection();
-            String sql = "SELECT USER_ID, USER_ALIAS, USER_NAME , HERO_EMAIL, USER_PHONE, TO_CHAR(JOIN_DATE, 'YYYY-MM-DD') JOIN_DATE, HERO_GRADE, USER_POINT, TRUNC(SYSDATE - JOIN_DATE) DAYS\n" +
+            String sql = "SELECT USER_ID, USER_ALIAS, USER_NAME, HERO_EMAIL, USER_PHONE, TO_CHAR(JOIN_DATE, 'YYYY-MM-DD') JOIN_DATE, HERO_GRADE, USER_POINT, TRUNC(SYSDATE - JOIN_DATE) DAYS\n" +
                     "FROM MEMBERS M JOIN GRADE G\n" +
                     "ON M.USER_POINT BETWEEN G.LO_POINT AND G.HI_POINT;\n";
             pstmt = conn.prepareStatement(sql);
@@ -41,10 +38,6 @@ public class MembersDao {
                  int days = rs.getInt("DAYS");
 
                 mvl.add(new MembersVO(userId,userAlias,userName,userEmail,userPhone,joinDate,heroGrade,userPoint,days));
-
-
-
-
 
             }
             Common.close(rs);
